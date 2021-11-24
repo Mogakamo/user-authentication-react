4611294873685cfb8a0015dbaa5e8fb06a8d78fa
Mogakamo <com02519@uoeld.ac.ke>
2021-11-24 11:37:47 +0300

    created the login page code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const username = "Mogaka"
+  const password = "password"
+
+  const [usernameState, setUsernameState] = useState("")
+  const [passwordState, setPasswordState] = useState("")
+  const [logIn, setlogIn] = useState(false)
+
+  const login = () => {
+    if (usernameState === username && passwordState === password) {
+      setlogIn(true)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="right">
+        <img src="https://source.unsplash.com/3MAmj1ZKSZA/1600x900" />
+      </div>
+      <div class="left">
+        <form>
+        <h1>Login</h1>
+        <label for="username"> Username</label>
+        <input type="text" onChange={(event) => {
+          setUsernameState(event.target.value)
+        }} required />
+        <label for="password"> Password</label>
+        <input type="password" onChange={(event) => {
+          setPasswordState(event.target.value)
+        }} required />
+        <input type="submit" onClick={login} value="Login" />
+
+        <p>Don't have an account? <a href="#">Register</a></p>
+        </form>
+
+
+        {logIn && 
+            <h1>Logged in as {username}</h1> 
+        }
+      </div>
     </div>
   );
 }
